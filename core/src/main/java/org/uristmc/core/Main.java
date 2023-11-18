11fcb2ed05c1876410a9acaa5f5a79db0951fe90
Zagir Dingizbaev <zagir.dingizbaev@waveaccess.ru>
2023-11-18 15:05:14 +0300

    Project init

---
+++ core/src/main/java/org/uristmc/core/Main.java
@@ -0,0 +1,11 @@
+package org.uristmc.core;
+
+/**
+ * @author Zagir Dingizbaev
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
