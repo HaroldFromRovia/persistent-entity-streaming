0aa1edc0d796bf1b122bd0afe8431e74e1ea2188
Zagir Dingizbaev <zagir.dingizbaev@waveaccess.ru>
2023-11-18 15:16:53 +0300

    Add main class

---
+++ vertx/src/main/java/org/uristmc/pes/vertx/Main.java
@@ -0,0 +1,11 @@
+package org.uristmc.pes.vertx;
+
+/**
+ * @author Zagir Dingizbaev
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
