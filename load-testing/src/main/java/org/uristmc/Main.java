09f11cc3bfe1fce11b3cb056706eeb491106229e
Zagir Dingizbaev <zagir.dingizbaev@waveaccess.ru>
2023-11-20 13:07:55 +0300

    create new basic modules

---
+++ load-testing/src/main/java/org/uristmc/Main.java
@@ -0,0 +1,11 @@
+package org.uristmc;
+
+/**
+ * @author Zagir Dingizbaev
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
